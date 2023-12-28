57572499e5f2337b10fbef7da6abce2559a27dd2
mdkhanga <khangaonkar@gmail.com>
2023-12-28 09:07:42 -0800

    Added map

--- main.go
+++ main.go
@@ -7,15 +7,32 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+var kvMap map[string]string
+
 func main() {
 	fmt.Println("Welcome to key value store")
 
+	kvMap = make(map[string]string)
+	kvMap["hello"] = "world"
+
 	router := gin.Default()
 	router.GET("/kvstore", getInfo)
+	router.GET("/kvstore/:key", getValue)
 
 	router.Run()
 }
 
 func getInfo(c *gin.Context) {
+	c.JSON(http.StatusOK, "Welcome to keystore")
+}
+
+func getValue(c *gin.Context) {
+	key := c.Param("key")
+	value := kvMap[key]
+	jsonString := fmt.Sprintf("{\"%s\":\"%s\"}", key, value)
+	c.JSON(http.StatusOK, jsonString)
+}
+
+func setValue(c *gin.Context) {
 	c.JSON(http.StatusOK, "Welcome to keystore")
 }
