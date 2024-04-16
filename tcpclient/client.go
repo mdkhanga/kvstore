4e53b7a7fbc527ba943ffd31353a15e87329e57e
mdkhanga <khangaonkar@gmail.com>
2024-04-15 17:10:11 -0700

    tcpclient

--- tcpclient/client.go
+++ tcpclient/client.go
@@ -9,6 +9,12 @@ func Connect(hostport string) (net.Conn, error) {
 
 	fmt.Println("Connecting to " + hostport)
 
-	return nil, nil
+	conn, err := net.Dial("tcp", "localhost:8080")
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+
+	return conn, nil
 
 }
