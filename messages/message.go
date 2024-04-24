a0219ab10e7d928e6d05534981df6d63d0e6c12d
mdkhanga <khangaonkar@gmail.com>
2024-04-23 17:35:31 -0700

    PingMessage

--- messages/message.go
+++ messages/message.go
@@ -1,11 +1,20 @@
 package messages
 
-type Message struct {
+import (
+	"bytes"
+	"encoding/binary"
+)
+
+type PingMessage struct {
 	Type MessageType
 	data *[]byte
 }
 
 type MessageType int16
+
+type Message interface {
+	Serialize() ([]byte, error)
+}
 
 const (
 	HELLO    MessageType = 0
@@ -13,6 +22,14 @@ const (
 	PING     MessageType = 2
 )
 
-func createPingMessage() *[]byte {
+func (message *PingMessage) serialize() ([]byte, error) {
 
+	buf := new(bytes.Buffer)
+
+	// Write the int32 value
+	if err := binary.Write(buf, binary.LittleEndian, message.Type); err != nil {
+		return nil, err
+	}
+
+	return buf.Bytes(), nil
 }
