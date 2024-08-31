0e2ab00952f33f4f30fdd7c5ba7e71d36fffb4c5
mdkhanga <khangaonkar@gmail.com>
2024-08-31 15:43:33 -0700

    save before trying grpc/proto

--- models/models.go
+++ models/models.go
@@ -1,6 +1,20 @@
 package models
 
+import (
+	"net"
+)
+
 type KeyValue struct {
 	Key   string
 	Value string
+}
+
+type ClusterMember struct {
+	host string
+	port int16
+	conn net.Conn
+}
+
+type Cluster struct {
+	members []ClusterMember
 }
