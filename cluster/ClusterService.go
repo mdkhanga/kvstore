47ed92fdbafe3436ff28fcaebc0058fdd333b34d
mdkhanga <khangaonkar@gmail.com>
2024-12-17 16:52:03 -0800

    start work on ClusterService

--- cluster/ClusterService.go
+++ cluster/ClusterService.go
@@ -5,3 +5,20 @@ import (
 )
 
 var clusterMap map[string]m.ClusterMember
+
+func AddToCluster(Hostname string, port int32) error {
+
+	return nil
+}
+
+func RemoveFromCluster(Hostname string, port int32) error {
+
+	return nil
+}
+
+func ListCluster() ([]m.ClusterMember, error) {
+
+	var members []m.ClusterMember
+
+	return members, nil
+}
